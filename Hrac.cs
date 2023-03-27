822e8e09cec5f884c5ff2f591694bb4f894ab2cc
MiroslavKuna <113085984+MiroslavKuna@users.noreply.github.com>
2023-03-27 08:25:04 +0200

    Add files via upload

---
+++ Hrac.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VlaknovaBezpecnost
+{
+    internal class Hrac
+    {
+        private string jmeno;
+        private int health;
+        private bool isAlive;
+
+        public Hrac(string jmeno, int health, bool isAlive)
+        {
+            Jmeno = jmeno;
+            Health = health;
+            IsAlive = isAlive;
+        }
+
+        public string Jmeno { get => jmeno; set => jmeno = value; }
+        public int Health { get => health; set => health = value; }
+        public bool IsAlive { get => isAlive; set => isAlive = value; }
+
+        public void Damage()
+        {
+            lock (this)
+            {
+                if (IsAlive)
+                {
+                    Health--;
+                }
+                return;
+            }
+        }
+
+        public void Heal()
+        {
+            lock (this)
+            {
+                if (IsAlive)
+                {
+                    health++;
+                }
+                return;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Health: " + health + "\nIs Alive? " + isAlive;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace VlaknovaBezpecnost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hrac h = new("hrac1", 100, true);
+            
+
+            Task t1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    h.Damage();
+                    Console.WriteLine(h.ToString());
+                }
+            });
+
+            Task t2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    h.Heal();
+                    Console.WriteLine(h.ToString());
+                }
+            });
+
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(h.ToString());
+            
+
+        }
+    }
+}
